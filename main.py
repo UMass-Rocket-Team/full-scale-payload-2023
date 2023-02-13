c4093d66ba6b87bd55842a94ddf88ae56ffefacb
dliousas <dliousas@umass.edu>
2023-02-13 18:24:26 -0500

    continuing refactoring

--- main.py
+++ main.py
@@ -40,15 +40,6 @@ def write_to_imu_data():
 imu_data_frequency = 100  # Hz
 imu_data_interval = 1 / imu_data_frequency * 1000  # ms
 
-
-# Declaring queues. They must be accessible via the global context
-time_queue = None
-accel_queue = None
-altitude_queue = None
-
-
-
-
 # -----CALIBRATION PHASE-----
 def calibration_fn():
     uart.write("Calibration required: sys {} gyro {} accel {} mag {}".format(*imu.cal_status()))
@@ -77,9 +68,8 @@ uart.write("\n\nIMU is calibrated")
 GRAVITY = 9.8
 ALTITUDE_THRESHOLD = 500  # ft
 queue_frequency = 5  # Hz
+accel_sample_interval = 1000 / queue_frequency
 check_interval = 5000  # time between variance checks
-
-# calculate time between samples in ms based on desired frequency
 
 data_updater = make_data_updater(queue_frequency, check_interval, (GRAVITY, ALTITUDE_THRESHOLD))
 
@@ -88,20 +78,23 @@ def find_reference_gravity():  # CHECK HERE FOR SOMEWHAT ARBITRARY VALUES
     if time.ticks_diff(time.ticks_ms(), time_queue.peek()) < check_interval * 0.5:
         return
     mean = accel_queue.getMean()
-    variance = accel_queue.getVariance()
-    if (variance < 0.1 and mean > 9.5 and mean < 10.1):  
+    
+    if (9.5 < mean < 10.1):
         # ARBITRARY: if variance less than .1 m/s^2, also check that mean is reasonable (g+-.3)
         # May also want to check that queue has big enough number of elements
         # Set reference gravity
-        flight_log.write("\nCalculated Gravity: " + str(mean))
-        uart.write("\nCalculated Gravity: " + str(mean))
-        GRAVITY = mean * 1.25 
-        # ARBITRARY: Multiply by 1.1 to give some room for error
-        raise StopIteration
+        variance = accel_queue.getVariance()
+        if variance < 0.1:
+            flight_log.write("\nCalculated Gravity: " + str(mean))
+            uart.write("\nCalculated Gravity: " + str(mean))
+            GRAVITY = mean * 1.25 
+            # ARBITRARY: Multiply by 1.1 to give some room for error
+            raise StopIteration
 
 
 do_every([data_updater, find_reference_gravity],[accel_sample_interval, check_interval])
-# GRAVITY is set at this point
+data_updater = None
+
 flight_log.write("\nGravity has been set: " + str(GRAVITY))
 flight_log.write("\nTime (ms): " + str(time_queue.peek()))
 uart.write("\n\nGravity has been calculated. Ready for flight.")
@@ -115,12 +108,8 @@ uart.write("\nTime (ms): " + str(time_queue.peek()))
 queue_frequency = 50  # Hz
 burn_time = 2970 * 0.85  # Multiply by .85 for some room for error
 accel_sample_interval = 1000 / queue_frequency
-# calculate time between samples in ms based on desired frequency
-max_size = calculate_max_size(queue_frequency, burn_time)
 
-time_queue = Queue(max_size, None)
-accel_queue = Queue(max_size, GRAVITY)
-altitude_queue = Queue(max_size, ALTITUDE_THRESHOLD)
+data_updater = make_data_updater(queue_frequency, burn_time, (GRAVITY, ALTITUDE_THRESHOLD))
 
 def check_launch():
     if time.ticks_diff(time.ticks_ms(), time_queue.peek()) < 0.5 * burn_time:
@@ -131,14 +120,12 @@ def check_launch():
     if altitude_queue.get_proportion_above_threshold() > 0.95:
         raise StopIteration  # ARBITRARY: If more than 95% of the data is above the altitude threshold, then the rocket has launched
 
-data_updater = make_data_updater(burn_time)
-# Function to update accelerations, keeping only last burn_time ms in the queue
 
 check_launch_interval = 1000  
 # ARBITRARY: check for launch every check_launch_interval ms
 
 do_every([write_to_imu_data, data_updater, check_launch], [imu_data_interval, accel_sample_interval, check_launch_interval])
-
+data_updater = None
 
 # The rocket has launched at this point
 flight_log.write("\nThe rocket has launched")
@@ -155,11 +142,8 @@ check_interval = 10000  # Want to keep track of last 10 seconds of acceleration 
 
 # calculate time between samples in ms based on desired frequency
 accel_sample_interval = 1000 / queue_frequency
-max_size = calculate_max_size(queue_frequency, check_interval)
 
-time_queue = Queue(max_size, None)
-accel_queue = Queue(max_size, GRAVITY)
-altitude_queue = Queue(max_size, ALTITUDE_THRESHOLD)
+data_updater = make_data_updater(queue_frequency, check_interval, (GRAVITY, ALTITUDE_THRESHOLD))
 
 def check_landing():
     if time.ticks_diff(time.ticks_ms(), time_queue.peek()) < 0.5 * check_interval:
@@ -173,7 +157,6 @@ def check_landing():
     # ARBITRARY: If less than 5% of the data is above the altitude threshold, then the rocket has landed
 
 
-data_updater = make_data_updater(check_interval)
 check_landing_interval = 3000  # ARBITRARY: Check for landing every 3 seconds
 do_every([write_to_imu_data, data_updater, check_landing], [imu_data_interval, accel_sample_interval, check_landing_interval])
 flight_log.write("\nThe rocket has landed")
