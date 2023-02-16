a827d21a6c306176832d438c11f7e0397898a74b
ravenspired <53878857+ravenspired@users.noreply.github.com>
2023-02-15 19:45:36 -0500

    Update motor_control.py

--- motor_control/motor_control.py
+++ motor_control/motor_control.py
@@ -1,21 +1,23 @@
-from machine import Pin, PWM
+from machine import Pin, PWM #import necessary packages
 from time import sleep
 
-IN1 = Pin(3, Pin.OUT)
+IN1 = Pin(3, Pin.OUT) #define pins for controlling motor direction
 IN2 = Pin(2, Pin.OUT)
 
-speed = PWM(Pin(4))
-speed.freq(1000)
+speed = PWM(Pin(4)) # define pin for controlling motor speed using PWM
+speed.freq(1000) # PWM frequency
 
+
+# info specific to motor being used
 # max speed: 60000
 # min speed: 30000
 
 
 
-def map_speed(speed_percentage):
+def map_speed(speed_percentage): # Takes values 0-100 as input and returns PWM values for use with the motor control
     return 30000 + 300 * speed_percentage
 
-def spin_forward(speed):
+def spin_forward(speed): # Speed controlled motor spinning forward
         s = int(map_speed(speed))
         speed = PWM(Pin(4))
         speed.freq(1000)
@@ -25,7 +27,7 @@ def spin_forward(speed):
         if speed == 0:
             brake()
 
-def spin_backward(speed):
+def spin_backward(speed): # Speed controlled motor spinning backward
         s = int(map_speed(speed))
         speed = PWM(Pin(4))
         speed.freq(1000)
@@ -35,18 +37,18 @@ def spin_backward(speed):
         if speed == 0:
             brake()
 
-def brake():
+def brake(): # Turn off the motor entirely
         print("brake pressed")
         IN1.low()  #stop
         IN2.low()
 
 
 
-for i in range(0,100):
+for i in range(0,100): #Sample loop demonstrating motor speed being controlled
     sleep(0.05)
     print(i)
     spin_forward(i)
 
-sleep(1)
-brake()
+sleep(1) # wait for a second
+brake() # turn off the motor
 
