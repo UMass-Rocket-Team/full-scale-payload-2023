4634305ab23f77e1fe4ee99f9678915c3c941c01
goobway <calistagreenway@gmail.com>
2023-02-15 23:13:39 -0500

    retry

--- payload.py
+++ payload.py
@@ -2,63 +2,81 @@ import cv2
 import os
 import datetime
 import time
-import serial
 
+import serial
 import RPi.GPIO as GPIO
+
 
 class CameraController:
     def __init__(self):
-        self.camera_index = self.find_available_camera()
-        self.grayscale = False
-    
-    def find_available_camera(self):
-        """
-        Finds the index of the available camera.
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(23, GPIO.OUT)
 
-        Returns:
-        int: The index of the available camera.
-        """
-        camera_index = 0
+        self.ser = serial.Serial(
+            port='/dev/ttyS0',
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
+
+        self.camera_index = self._find_available_camera()
+
+        self.grayscale_mode = False
+
+    def _find_available_camera(self):
         for i in range(10):
             cap = cv2.VideoCapture(i)
             if cap.isOpened():
                 print(f"Camera with index {i} is available.")
                 cap.release()
-                camera_index = i
-                break
-        return camera_index
+                return i
+        return 0
 
-    def turn_camera(self, degrees):
-        """
-        Turns the camera by a certain number of degrees.
+    def _wait_for_command(self):
+        no_command = True
 
-        Args:
-        degrees (int): The number of degrees to turn the camera.
-        """
-        # Send message to Pico to turn camera
+        while no_command:
+            msg = self.ser.readline().strip().decode()
+            if len(msg) == 36:
+                no_command = False
+
+        return msg
+
+    def _process_command(self, command):
+        if command == "A1":
+            self._turn_camera(60)
+        elif command == "B2":
+            self._turn_camera(-60)
+        elif command == "C3":
+            self._take_picture()
+        elif command == "D4":
+            self.grayscale_mode = True
+        elif command == "E5":
+            self.grayscale_mode = False
+        elif command == "F6":
+            self._rotate_image(180)
+        elif command == "G7":
+            self._apply_special_effects()
+        elif command == "H8":
+            self._remove_all_filters()
+
+    def _turn_camera(self, degrees):
+        # Send message to Pico
+        # Turn camera to the specified degrees
         pass
 
-    def take_picture(self):
-        """
-        Takes a picture using the camera and saves it to a folder.
-        """
+    def _take_picture(self):
         camera = cv2.VideoCapture(self.camera_index)
-        # Wait for 5 seconds before taking image
         time.sleep(5)
-        # Read the image from the camera
         ret, frame = camera.read()
-        # If image is successfully read
         if ret:
-            # Convert to grayscale if grayscale mode is on
-            if self.grayscale:
+            if self.grayscale_mode:
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            # Get the current timestamp
             timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            # Construct the image path, timestamp, and save image
-            image_folder = "images"
-            if not os.path.exists(image_folder):
-                os.makedirs(image_folder)
-            image_path = os.path.join(image_folder, f"image_{timestamp}.jpg")
+            image_path = os.path.join("images", f"image_{timestamp}.jpg")
             cv2.putText(frame, timestamp, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
             cv2.imwrite(image_path, frame)
             print(f"Image taken and stored as {image_path}")
@@ -66,48 +84,33 @@ class CameraController:
             print("Failed to take image.")
         camera.release()
 
-    def set_grayscale_mode(self, is_grayscale):
-        """
-        Sets the grayscale mode on or off.
+    def _rotate_image(self, degrees):
+        # Rotate the most recent image by the specified degrees
+        pass
 
-        Args:
-        is_grayscale (bool): Whether to set the grayscale mode on or off.
-        """
-        self.grayscale = is_grayscale
+    def _apply_special_effects(self):
+        # Apply special effects to the most recent image
+        pass
 
-class XBeeController:
-    def __init__(self, call_sign):
-        self.call_sign = call_sign
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-        GPIO.setup(23, GPIO.OUT)
-        self.ser = serial.Serial(
-            port = '/dev/ttyS0',
-            baudrate = 9600,
-            parity = serial.PARITY_NONE,
-            stopbits = serial.STOPBITS_ONE,
-            bytesize = serial.EIGHTBITS,
-            timeout = 1
-        )
-        self.wait_for_rafco_transmission()
+    def _remove_all_filters(self):
+        # Remove all filters from the most recent image
+        pass
 
-    def wait_for_rafco_transmission(self):
-        """
-        Waits for RAFCO transmission before continuing.
-        """
-        no_command = True
-        while no_command:
-            msg = self.ser.readline().strip().decode()
-            if len(msg) == 36:
-                no_command = False
+    def run(self):
+        msg = self._wait_for_command()
+        call_sign = "XX4XXX"
+        codes = msg.split()
 
-    def decode_message(self, msg):
-        """
-        Decodes the message sent by the XBee module.
+        if codes[0] != call_sign:
+            print("Invalid call sign.")
+            exit()
 
-        Args:
-        msg (str): The message sent by the XBee module.
+        codes = codes[1:]
 
-        Returns:
-        list: The list of
-        """
+        for code in codes:
+            if code in code_to_command:
+                command = code_to_command[code]
+                print(f"Command given: {command}")
+                self._process_command(code)
+            else:
+               pass
