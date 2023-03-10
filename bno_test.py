a5fd00930d47f008ce684cae2a3b1648ca9fa412
dliousas <dliousas@umass.edu>
2023-03-09 20:32:43 -0500

    deleting extra files

--- bno_test.py
+++
@@ -1,8 +0,0 @@
-import adafruit_bno055
-import board
-import time
-i2c = board.I2C()
-sensor = adafruit_bno055.BNO055_I2C(i2c)
-while True:
-    print(sensor.acceleration)
-    time.sleep(.01)
--- servo/pico_message.py
+++
@@ -1,8 +0,0 @@
-import serial
-# import pico.example as pico
-
-ser = serial.Serial("/dev/ttyACM0", 115200)
-while True:
-    message = "Hello World!"
-    ser.write(message.encode())
-ser.close()
